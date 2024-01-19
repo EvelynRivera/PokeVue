5792b2fb324607b8386514118708470be52d02e7
EvelynGabrielaRivera <evelyn@davg.dev>
2024-01-18 23:56:38 -0800

    feature: Conexión Axios

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
 <template>
   <h1>Hola Mundo</h1>
 </template>
+<script setup lang="ts">
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+
+   const app = ref()
+   info: []
+
+  onMounted(() => {
+    axios
+    .get('https://pokeapi.co/api/v2/pokemon/squirtle')
+    .then(response => {this.info = response.data.pokemon })
+    console.log(response);
+  })
+
+
+
+</script>
